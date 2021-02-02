23dd8ac446da02fc51054d847332f38fafe18fb3
edougher <ejdougher@gmail.com>
2021-02-01 22:18:49 -0600

    commit tree 38f31ad343e27e6880a74ef96f6dd1eb278c5a99

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+
+let dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+    //gameBox is 400px x 400px
+    //dodger is 40px wide
+    //so stop if the left edge is 40 less than 400 (360)
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
